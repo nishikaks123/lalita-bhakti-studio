425f1c40bd194364a7e440a6745b79b523954abe
Kartavya Soni <kartavyasoni175@gmail.com>
2025-09-02 12:54:01 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,15 +4,15 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  databaseURL: `https://lalita-bhakti-studio-default-rtdb.firebaseio.com`
+  apiKey: "AIzaSyDFLoIpHfiVJDT6X0lfLkjANMoncHwLXPo",
+  authDomain: "lalita-bhakti-studio.firebaseapp.com",
+  projectId: "lalita-bhakti-studio",
+  storageBucket: "lalita-bhakti-studio.appspot.com",
+  messagingSenderId: "314080639188",
+  appId: "1:314080639188:web:6bc46bc49e3496c659a341",
+  databaseURL: "https://lalita-bhakti-studio-default-rtdb.firebaseio.com"
 };
+
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
